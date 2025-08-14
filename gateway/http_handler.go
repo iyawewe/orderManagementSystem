227b8eaf0bd7b4c5033c14468350bb8b31436a50
loveneet singh <lilfootballer14@gmail.com>
2025-08-14 17:50:39 +0530

    error handing

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/iyawewe/orderManagementSystem/common/api"
@@ -14,8 +15,8 @@ type handler struct {
 func NewHandler(client pb.OrderServiceClient) *handler {
 	return &handler{client}
 }
-func (h *handler) registerRoutes(mux *http.ServeMux) {
 
+func (h *handler) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/customers/{customerID}/orders", h.HandlerCreateOrder)
 }
 
@@ -28,8 +29,34 @@ func (h *handler) HandlerCreateOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+	if err := validateItems(items); err != nil {
+		api.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	o, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
 		CustomerID: customerID,
 		Items:      items,
 	})
+	if err != nil {
+		api.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	api.WriteJSON(w, http.StatusOK, o)
+}
+
+func validateItems(items []*pb.ItemsWithQuantity) error {
+	if len(items) == 0 {
+		return errors.New("items must have at least one item")
+	}
+	for _, i := range items {
+		if i.Id == "" {
+			return errors.New("item ID is required")
+		}
+		if i.Quantity <= 0 {
+			return errors.New("item quantity must be greater than 0")
+		}
+	}
+	return nil
 }
